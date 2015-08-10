e19a8e795a4343f1f730b37a30d38ec29a006439
Tonsai Singhkangwan <t@iton.in.th>
2015-08-11 03:22:26 +0700

    Hide question order by default

--- resources/views/backend/question/index.blade.php
+++ resources/views/backend/question/index.blade.php
@@ -70,9 +70,12 @@
 
 <div class="row">
 	<div class="col-xs-12">
-		<div class="box">
+		<div class="box collapsed-box">
 			<div class="box-header with-border">
 				<h3 class="box-title"><i class="fa fa-sort-amount-asc"></i>ตัวอย่างลำดับการแสดงคำถามในใบสมัคร</h3>
+				<div class="box-tools">
+					<button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+				</div>
 			</div>
 			<div class="box-body no-padding">
 				<div class="table-responsive">
