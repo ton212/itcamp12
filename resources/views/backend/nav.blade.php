ff5609952abda64ae28cfd6c4c9d3ae603c865d0
Nathawut Worakijlawan <topgunz.889@gmail.com>
2015-09-04 21:00:27 +0700

    Add Images exchange link more

--- resources/views/backend/nav.blade.php
+++ resources/views/backend/nav.blade.php
@@ -22,9 +22,9 @@
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> {{ Auth::user()->name }} <span class="caret"></span></a>
 						<ul class="dropdown-menu" role="menu">
-							<li><a href="{{ URL::to('/profile/edit') }}"><span class="fa fa-pencil"></span>แก้ไขข้อมูลส่วนตัว</a></li>
-							<li><a href="{{ URL::to('/profile/password') }}"><span class="fa fa-wrench"></span>เปลี่ยนรหัสผ่าน</a></li>
-							<li class="divider"></li>
+							<!--<li><a href="{{ URL::to('/profile/edit') }}"><span class="fa fa-pencil"></span>แก้ไขข้อมูลส่วนตัว</a></li>-->
+							<!--<li><a href="{{ URL::to('/profile/password') }}"><span class="fa fa-wrench"></span>เปลี่ยนรหัสผ่าน</a></li>-->
+							<!--<li class="divider"></li>-->
 							<li><a href="{{ URL::to('/backend/auth/logout') }}"><span class="glyphicon glyphicon-log-out text-danger"></span><span class="text-danger">ออกจากระบบ</span></a></li>
 						</ul>
 					</li>
--- resources/views/link-exchange/create.blade.php
+++ resources/views/link-exchange/create.blade.php
@@ -24,36 +24,77 @@
 				    </div>
 				  </div>
 				</form>
-			
-		
 		<hr>
-		<div class="text-center">
-		<h2>ภาพประชาสัมพันธ์</h2><br>
-		</div>
-		<div class="text-center">
-		<img src="{{URL::to('banner/bn_500x500.jpg')}}" width="125" height="125">
-		</div>
-		<p>ขนาด 125x125</p>
-		<textarea class="form-control" style="font-size:18px;" onclick="this.select();">&lt;a href="http://www.itcamp.in.th"&gt;&lt;img src="{{URL::to('/banner/bn_500x500.jpg')}}" width="125" height="125" border="0" alt="itcamp#12"&gt;&lt;/a&gt;</textarea>
-		<br>
-		<div class="text-center">
-		<img src="{{URL::to('banner/bn_500x500.jpg')}}" width="250" height="250">
-		</div>
-		<p>ขนาด 250x250</p>
-		<textarea class="form-control" style="font-size:18px;" onclick="this.select();">&lt;a href="http://www.itcamp.in.th"&gt;&lt;img src="{{URL::to('/banner/bn_500x500.jpg')}}" width="250" height="250" border="0" alt="itcamp#12"&gt;&lt;/a&gt;</textarea>
-		<br>
-		<div class="text-center">
-		<img src="{{URL::to('banner/bn_500x500.jpg')}}" width="500" height="500">
-		</div>
-		<p>ขนาด 500x500</p>
-		<textarea class="form-control" style="font-size:18px;" onclick="this.select();">&lt;a href="http://www.itcamp.in.th"&gt;&lt;img src="{{URL::to('/banner/bn_500x500.jpg')}}" width="500" height="500" border="0" alt="itcamp#12"&gt;&lt;/a&gt;</textarea>
-		<br>
-		<div class="text-center">
-		<img src="{{URL::to('banner/bn_600x1200.jpg')}}" width="300" height="600">
-		</div>
-		<p>ขนาด 300x600</p>
-		<textarea class="form-control" style="font-size:18px;" onclick="this.select();">&lt;a href="http://www.itcamp.in.th"&gt;&lt;img src="{{URL::to('/banner/bn_600x1200.jpg')}}" width="300" height="600" border="0" alt="itcamp#12"&gt;&lt;/a&gt;</textarea>
+		 </div>
+		 </div>
+		 <div class="row">
+				<div class="text-center">
+					<h2>ภาพประชาสัมพันธ์</h2><br>
+				</div>
+			<div class="text-center">
+				<img src="{{URL::to('banner/bn_500x500.jpg')}}" width="125" height="125">
+			</div>
+			<div class="col-md-8 col-md-offset-2">	
+				<p>ขนาด 125x125</p>
+				<textarea class="form-control " style="font-size:18px;" onclick="this.select();">&lt;a href="http://www.itcamp.in.th"&gt;&lt;img src="{{URL::to('/banner/bn_500x500.jpg')}}" width="125" height="125" border="0" alt="itcamp#12"&gt;&lt;/a&gt;</textarea>
+				<br>
+			</div>
+			<div class="text-center">
+				<img src="{{URL::to('banner/bn_500x500.jpg')}}" width="250" height="250">
+			</div>
+			<div class="col-md-8 col-md-offset-2">
+				<p>ขนาด 250x250</p>
+				<textarea class="form-control" style="font-size:18px;" onclick="this.select();">&lt;a href="http://www.itcamp.in.th"&gt;&lt;img src="{{URL::to('/banner/bn_500x500.jpg')}}" width="250" height="250" border="0" alt="itcamp#12"&gt;&lt;/a&gt;</textarea>
+				<br>
+			</div>
+			<div class="text-center">
+				<img src="{{URL::to('banner/bn_500x500.jpg')}}" width="500" height="500">
+			</div>
+			<div class="col-md-8 col-md-offset-2">
+				<p>ขนาด 500x500</p>
+				<textarea class="form-control" style="font-size:18px;" onclick="this.select();">&lt;a href="http://www.itcamp.in.th"&gt;&lt;img src="{{URL::to('/banner/bn_500x500.jpg')}}" width="500" height="500" border="0" alt="itcamp#12"&gt;&lt;/a&gt;</textarea>
+				<br>
+			</div>
+			<div class="text-center">
+				<img src="{{URL::to('banner/bn_600x1200.jpg')}}" width="300" height="600">
+			</div>
+			<div class="col-md-8 col-md-offset-2">
+				<p>ขนาด 300x600</p>
+				<textarea class="form-control" style="font-size:18px;" onclick="this.select();">&lt;a href="http://www.itcamp.in.th"&gt;&lt;img src="{{URL::to('/banner/bn_600x1200.jpg')}}" width="300" height="600" border="0" alt="itcamp#12"&gt;&lt;/a&gt;</textarea>
+				<br>
+			</div>
+			<div class="text-center">
+				<img src="{{URL::to('banner/bn_1100x724.jpg')}}" width="550" height="362">
+			</div>
+			<div class="col-md-8 col-md-offset-2">
+				<p>ขนาด 550x362</p>
+				<textarea class="form-control" style="font-size:18px;" onclick="this.select();">&lt;a href="http://www.itcamp.in.th"&gt;&lt;img src="{{URL::to('/banner/bn_1100x724.jpg')}}" width="550" height="362" border="0" alt="itcamp#12"&gt;&lt;/a&gt;</textarea>
+				<br>
+			</div>
+			<div class="text-center">
+				<img src="{{URL::to('banner/bn_1120x580.jpg')}}" width="560" height="290">
+			</div>
+			<div class="col-md-8 col-md-offset-2">
+				<p>ขนาด 560x290</p>
+				<textarea class="form-control" style="font-size:18px;" onclick="this.select();">&lt;a href="http://www.itcamp.in.th"&gt;&lt;img src="{{URL::to('/banner/bn_1120x580.jpg')}}" width="560" height="290" border="0" alt="itcamp#12"&gt;&lt;/a&gt;</textarea>
+				<br>
+			</div>
+			<div class="text-center">
+				<img src="{{URL::to('banner/bn_1600x300.jpg')}}" width="800" height="150">
+			</div>
+			<div class="col-md-8 col-md-offset-2">
+				<p>ขนาด 800x300</p>
+				<textarea class="form-control" style="font-size:18px;" onclick="this.select();">&lt;a href="http://www.itcamp.in.th"&gt;&lt;img src="{{URL::to('/banner/bn_1600x300.jpg')}}" width="800" height="150" border="0" alt="itcamp#12"&gt;&lt;/a&gt;</textarea>
+				<br>
+			</div>
+			<div class="text-center">
+				<img src="{{URL::to('banner/bn_1920x640.jpg')}}" width="960" height="320">
+			</div>
+			<div class="col-md-8 col-md-offset-2">
+				<p>ขนาด 960x320</p>
+				<textarea class="form-control" style="font-size:18px;" onclick="this.select();">&lt;a href="http://www.itcamp.in.th"&gt;&lt;img src="{{URL::to('/banner/bn_1920x640.jpg')}}" width="960" height="320" border="0" alt="itcamp#12"&gt;&lt;/a&gt;</textarea>
+			</div>
 	</div>
 	</div>
-	</div>
+
 @endsection
